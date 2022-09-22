49053c5e7d3f3e29e977ab385b18681979d0aa24
Ozan <ozan.yasin97@gmail.com>
2022-09-22 17:00:11 +0300

    functionsExercise END

--- Functions/functionsExercise.js
+++ Functions/functionsExercise.js
@@ -4,33 +4,27 @@
 // Write a function called "twoFer" that accepts a person's name
 // It should return a string in the format "one for <name>, one for me"
 // If no name is provided, it should default to "you"
-
 // twoFer() => "One for you, one for me"
 // twoFer("Elton") => "One for Elton, one for me"
-
-function twoFer(person: string = 'you'): string {
-  return `one for ${person}, one for me`;
+function twoFer(person) {
+    if (person === void 0) { person = 'you'; }
+    return "one for ".concat(person, ", one for me");
 }
-
 console.log(twoFer());
 console.log(twoFer('Elton'));
-
 // **********************************************
 // ******************* PART 2 *******************
 // **********************************************
 // Write a isLeapYear() function that accepts a year and returns true/false depending on if the year is a leap year
 // isLeapYear(2012) => true
 // isLeapYear(2013) => false
-
 // To determine whether a year is a leapYear, use this "formula":
 // A YEAR IS A LEAPYEAR IF
 // - year is a multiple of 4 AND not a multiple of 100
 // OR...
 // - year is a multiple of 400
 // hint - use modulo
-
 // Naive Solution
-
 // const isLeapYear = (year: number): boolean => {
 //   if (year % 4 === 0 && year % 100 !== 0) {
 //     return true;
@@ -39,14 +33,9 @@ console.log(twoFer('Elton'));
 //   }
 //   return false;
 // };
-
 // Refactored
-
-const isLeapYear = (year: number): boolean => {
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+var isLeapYear = function (year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
-
 console.log(2012);
 console.log(2013);
-
-// terminal => tsc functionsExercise.ts
