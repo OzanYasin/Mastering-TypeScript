9d111f280dd2524222356fee35d52f2a63531531
Ozan <ozan.yasin97@gmail.com>
2022-09-23 13:47:59 +0300

    TypeScript - Array Types updated

--- ArrayTypes/index.ts
+++ ArrayTypes/index.ts
@@ -1,4 +1,4 @@
-// Array Types
+// !! Array Types !!
 
 // Arrays can be typed using a type annotation followed by empty array brackets, like number[] for an array of numbers.
 
@@ -12,3 +12,27 @@ activeUsers.push('Tony'); // Ok
 const ageList: number[] = [45, 33, 56, 13];
 ageList[0] = 99; // Ok
 // ageList[0] = 'hi' // Error
+
+// ----------------------------------
+
+// !! More Array Syntax !!
+
+// const bools: Array<boolean> = []
+const bools: boolean[] = [];
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const coords: Point[] = [];
+coords.push({ x: 23, y: 4 });
+
+const board: string[][] = [
+  // It means two dimensional array.
+  ['X', 'O', 'X'],
+  ['X', 'O', 'X'],
+  ['X', 'O', 'X'],
+];
+
+const demo: number[][][] = [[[1]]]; // Three dimensional array.
