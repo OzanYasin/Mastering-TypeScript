a97c8cf4ced7e0554e0a336c9a4050f8605bd477
Ozan <ozan.yasin97@gmail.com>
2022-09-23 18:47:18 +0300

    TS - Union Types START

---
+++ UnionTypes/index.ts
@@ -0,0 +1,5 @@
+// !! Union Types !!
+
+// Union types allow us to give a few different possible types. If the eventual value's type is included, TS will be happy :).
+
+// We can create a union type by using the single | (pipe character) to separate the types we want to include. It's like saying, "This thing is allowed to be this, this, or this". TS will enforce it from there.
