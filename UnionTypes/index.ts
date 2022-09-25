e93d209bd96abfef5db446319a05aa951fb076c6
Ozan <ozan.yasin97@gmail.com>
2022-09-25 20:50:25 +0300

    TS - Union Types updated

--- UnionTypes/index.ts
+++ UnionTypes/index.ts
@@ -3,3 +3,20 @@
 // Union types allow us to give a few different possible types. If the eventual value's type is included, TS will be happy :).
 
 // We can create a union type by using the single | (pipe character) to separate the types we want to include. It's like saying, "This thing is allowed to be this, this, or this". TS will enforce it from there.
+
+let age: number | string = 21;
+age = 23;
+age = '24';
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type Loc = {
+  lat: number;
+  long: number;
+};
+
+let coordinates: Point | Loc = { x: 1, y: 34 };
+coordinates = { lat: 312.354, llong: 23.321 };
