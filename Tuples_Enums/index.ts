f1b38abc90696b27fd5c68055c50382f76b1fcfa
Ozan <ozan.yasin97@gmail.com>
2022-09-27 14:24:28 +0300

    TS - Tuples & Enums START

---
+++ Tuples_Enums/index.ts
@@ -0,0 +1,6 @@
+// !! Tuples !!
+
+// Tuples are special type exclusive to TS
+// * They don't exist in JS
+
+// Tuples are arrays of fixed lengths and ordered with specific types - like super rigid arrays.
