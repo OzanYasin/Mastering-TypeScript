5c7e973a967e462a59b246768385729ae068a01e
Ozan <ozan.yasin97@gmail.com>
2022-09-27 15:41:58 +0300

    TS - interfaces STARt

---
+++ 07_Interfaces/index.ts
@@ -0,0 +1,8 @@
+// !! Interfaces !!
+
+// Interfaces serve almost the exact same purpose as type aliases
+// (with a slightly different syntax)
+
+// We can use them to create reusable, modular types that describe the shapes of objects.
+
+// * Interfaces are the most commonly used pieces of TypeScript.
