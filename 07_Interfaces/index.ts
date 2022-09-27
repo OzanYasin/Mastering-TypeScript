4fe97d07a0279205320d0693a3d31be3237d9134
Ozan <ozan.yasin97@gmail.com>
2022-09-27 15:52:00 +0300

    TS - interfaces updated

--- 07_Interfaces/index.ts
+++ 07_Interfaces/index.ts
@@ -6,3 +6,34 @@
 // We can use them to create reusable, modular types that describe the shapes of objects.
 
 // * Interfaces are the most commonly used pieces of TypeScript.
+
+//An interface
+interface Person {
+  name: string;
+  age: number;
+}
+
+//Use the type alias in the annotation
+const sayHappyBirthday = (person: Person) => {
+  return `Hey ${person.name}, congrats on turning on ${person.age}!`;
+};
+
+sayHappyBirthday({ name: 'Henry', age: 24 });
+
+// * Both Type Alias & Interfaces are very similar. But we will discuss it at the end of this section.
+
+// Interfaces allow us to describe the shape of objects and only objects.
+
+// ** Can not use interface for like a union type.
+
+interface Point {
+  // Remember, this not an object, it's curly braces, but it's TS.
+  x: number;
+  y: number;
+}
+
+const pt: Point = { x: 123, y: 12 };
+
+// ----------------------------------
+
+// !! Readonly & Optional Interface Properties !!
