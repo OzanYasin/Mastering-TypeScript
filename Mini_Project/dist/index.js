6fadef094300fae345f7b658f107c66bc30c893c
Ozan <ozan.yasin97@gmail.com>
2022-09-30 16:20:04 +0300

    TS - Mini Project UPDATED

--- Mini_Project/dist/index.js
+++ Mini_Project/dist/index.js
@@ -7,3 +7,25 @@
 // For example, you might want to work with Node. In that case you have no use for any of this defined document type stuff.
 // !! Lib !!
 // TypeScript includes a default set of type definitions for built-in JS APIs (like Math), as well as type definitions for things found in browser environments (like document).
+// ----------------------------
+// Exclamation point (!)
+// If we put exclamation point after code line of below, this tells TS stop worrying, this is guaranteed not to be null.
+// Only would use this in situations where you know for sure that a value will be found or it won't be null.
+var btn = document.getElementById('btn');
+btn.addEventListener;
+// ----------------------------
+// !! Type Assertions !!
+var mystery = 'Hello World!';
+// const numChars = mystery.length // Error
+// This is where we can use the type assertion syntax.
+var numChars = mystery.length;
+// Let's talk about real world example of "Type Assertions"
+var input = document.getElementById('todoInput');
+// We need to use a type assertion there to tell TypeScript that this is an HTML Input Element, not just a generic element.
+btn.addEventListener('click', function () {
+    console.log(input.value);
+    input.value = '';
+});
+// * Alternative way of Type Assertion
+// (<HTMLInputElement>input).value;
+// *** It's less common in general but does not work with JSX.
--- Mini_Project/src/index.ts
+++ Mini_Project/src/index.ts
@@ -12,3 +12,43 @@
 // !! Lib !!
 
 // TypeScript includes a default set of type definitions for built-in JS APIs (like Math), as well as type definitions for things found in browser environments (like document).
+
+// ----------------------------
+
+// Exclamation point (!)
+// If we put exclamation point after code line of below, this tells TS stop worrying, this is guaranteed not to be null.
+
+// Only would use this in situations where you know for sure that a value will be found or it won't be null.
+
+const btn = document.getElementById('btn')!;
+
+btn.addEventListener;
+
+// ----------------------------
+
+// !! Type Assertions !!
+
+let mystery: unknown = 'Hello World!';
+
+// const numChars = mystery.length // Error
+
+// This is where we can use the type assertion syntax.
+
+const numChars = (mystery as string).length;
+
+// Let's talk about real world example of "Type Assertions"
+
+const input = document.getElementById('todoInput')! as HTMLInputElement;
+
+// We need to use a type assertion there to tell TypeScript that this is an HTML Input Element, not just a generic element.
+
+btn.addEventListener('click', () => {
+  console.log(input.value);
+  input.value = '';
+});
+
+// * Alternative way of Type Assertion
+
+// (<HTMLInputElement>input).value;
+
+// *** It's less common in general but does not work with JSX.
