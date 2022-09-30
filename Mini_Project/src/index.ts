4f9fe914f40a63060442d3f0bfc92db24890922b
Ozan <ozan.yasin97@gmail.com>
2022-09-30 15:31:32 +0300

    TS - Project UPDATED

---
+++ Mini_Project/src/index.ts
@@ -0,0 +1,14 @@
+// Working with the DOM
+
+// TypeScript does know about the document, it does know about DOM elements and all the different element types and add event listener and query selector.
+
+// document -> type definition, it's shown every type of document object.
+
+// * There is an option to turn that off.
+// (if not working with the DOM)
+
+// For example, you might want to work with Node. In that case you have no use for any of this defined document type stuff.
+
+// !! Lib !!
+
+// TypeScript includes a default set of type definitions for built-in JS APIs (like Math), as well as type definitions for things found in browser environments (like document).
