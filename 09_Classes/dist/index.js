8503d67232a7d9bf8ab41b4b1f2252b5ac1a54af
Ozan <ozan.yasin97@gmail.com>
2022-10-03 21:50:34 +0300

    TS - Classes UPDATED

--- 09_Classes/dist/index.js
+++ 09_Classes/dist/index.js
@@ -1,12 +1,30 @@
 "use strict";
 class Player {
-    constructor(first, last) {
-        this.score = 0;
+    // public readonly first: string;
+    // public readonly last: string;
+    // public score: number = 0;
+    constructor(first, last, _score = 0 // Parameter Properties (Also known as shortcut syntax)
+    ) {
         this.first = first;
         this.last = last;
+        this._score = _score;
+    }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error('Score cannot be negative');
+        }
+        this._score = newScore;
     }
 }
 const elton = new Player('Elton', 'Steele');
+// elton.fullName = 'Something Else' // It's readonly on default. So, it throws Error
+elton.score = 33;
 // readonly -> Does not allow to change the property or method
 // public -> It makes clear that property or method is reachable from outside.
 // private -> It makes property or method is only accessible or usable inside the class.
--- 09_Classes/src/index.ts
+++ 09_Classes/src/index.ts
@@ -1,15 +1,32 @@
 class Player {
-  public readonly first: string;
-  public readonly last: string;
-  public score: number = 0;
+  // public readonly first: string;
+  // public readonly last: string;
+  // public score: number = 0;
 
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
+  constructor(
+    public first: string,
+    public last: string,
+    private _score: number = 0 // Parameter Properties (Also known as shortcut syntax)
+  ) {}
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error('Score cannot be negative');
+    }
+    this._score = newScore;
   }
 }
 
 const elton = new Player('Elton', 'Steele');
+// elton.fullName = 'Something Else' // It's readonly on default. So, it throws Error
+elton.score = 33;
 
 // readonly -> Does not allow to change the property or method
 // public -> It makes clear that property or method is reachable from outside.
