ae4ac9de8eeff3cec8086ca2c3d38815d2255ee6
Ozan <ozan.yasin97@gmail.com>
2022-10-03 21:31:58 +0300

    TS - Classes UPDATED

--- 09_Classes/src/index.ts
+++ 09_Classes/src/index.ts
@@ -1,1 +1,16 @@
-console.log('IT WORKS');
+class Player {
+  public readonly first: string;
+  public readonly last: string;
+  public score: number = 0;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+}
+
+const elton = new Player('Elton', 'Steele');
+
+// readonly -> Does not allow to change the property or method
+// public -> It makes clear that property or method is reachable from outside.
+// private -> It makes property or method is only accessible or usable inside the class.
