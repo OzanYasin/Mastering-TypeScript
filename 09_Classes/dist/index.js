46441fa4cd99c3349b82937a148ee690989b27c9
Ozan <ozan.yasin97@gmail.com>
2022-10-03 20:24:25 +0300

    Mastering TypeScript - Classes UPDATED

--- 09_Classes/dist/index.js
+++ 09_Classes/dist/index.js
@@ -1,2 +1,28 @@
-"use strict";
-console.log('IT WORKS');
+// A Recap of JS Classes
+
+// Classes are templates for creating objects in JavaScript. They contain a few different important pieces which allow for creation and extension of customized (and nicely organized) objects.
+
+class Player {
+  #score = 0;
+  #numLives = 10;
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
+  getScore() {
+    return this.#score;
+  }
+  updateScore(newScore) {
+    this.#score = newScore;
+  }
+  loseLife() {
+    this.numLives -= 1;
+  }
+  #secret() {
+    console.log('SECRET MESSAGE!');
+  }
+}
+
+const player1 = new Player('blue', 'steele');
+
+// console.log(player1.#secret) // Error
