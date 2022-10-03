f07e3f8f5256b562a51f9781d3e5f39fa1238b10
Ozan <ozan.yasin97@gmail.com>
2022-10-03 20:53:47 +0300

    TS - Classes UPDATED

--- 09_Classes/dist/index.js
+++ 09_Classes/dist/index.js
@@ -3,14 +3,30 @@
 // Classes are templates for creating objects in JavaScript. They contain a few different important pieces which allow for creation and extension of customized (and nicely organized) objects.
 
 class Player {
+  // static tells JS that property or method exist on the class itself and is not going to to exist on each individual instance.
+  static description = 'Player In Our Game';
   #score = 0;
   #numLives = 10;
   constructor(first, last) {
     this.first = first;
     this.last = last;
   }
-  getScore() {
+  get fullName() {
+    return `${this.first} ${this.last}`;
+  }
+  set fullName(newName) {
+    const [first, last] = newName.split(' ');
+    this.first = first;
+    this.last = last;
+  }
+  get score() {
     return this.#score;
+  }
+  set score(newScore) {
+    if (newScore < 0) {
+      throw new Error('Score must be positive!');
+    }
+    this.#score = newScore;
   }
   updateScore(newScore) {
     this.#score = newScore;
@@ -23,6 +39,16 @@ class Player {
   }
 }
 
+class AdminPlayer extends Player {
+  isAdmin = true;
+}
+
+const admin = new AdminPlayer();
+
 const player1 = new Player('blue', 'steele');
 
 // console.log(player1.#secret) // Error
+console.log(player1.fullName);
+player1.fullName = 'Ozan Yasin';
+
+console.log(player1);
