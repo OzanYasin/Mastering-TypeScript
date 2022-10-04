ac353346d83f0d997204dad56adfcbce3c49fe78
Ozan <ozan.yasin97@gmail.com>
2022-10-04 22:44:09 +0300

    TS - Generics UPDATED

--- 10_Generics/dist/index.js
+++ 10_Generics/dist/index.js
@@ -9,3 +9,31 @@ const inputEl = document.querySelector('#username');
 console.dir(inputEl);
 inputEl.value = 'Hacked!';
 const btn = document.querySelector('.btn');
+// -------------------------------
+// !! Writing Our First Generic !!
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+// function identity(item: any): any {
+//   return item;
+// }
+// Above functions are not coming handy.
+// Generic functions are where it comes become useful.
+//Syntax
+function identity(item) {
+    return item;
+}
+identity(7);
+identity('hello');
+//Common Language
+function identity1(item) {
+    return item;
+}
+// -------------------------------
+// !! Writing Another Generic Function !!
--- 10_Generics/src/index.ts
+++ 10_Generics/src/index.ts
@@ -13,3 +13,44 @@ console.dir(inputEl);
 inputEl.value = 'Hacked!';
 
 const btn = document.querySelector<HTMLButtonElement>('.btn')!;
+
+// -------------------------------
+
+// !! Writing Our First Generic !!
+
+function numberIdentity(item: number): number {
+  return item;
+}
+
+function stringIdentity(item: string): string {
+  return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+  return item;
+}
+
+// function identity(item: any): any {
+//   return item;
+// }
+
+// Above functions are not coming handy.
+
+// Generic functions are where it comes become useful.
+
+//Syntax
+function identity<Type>(item: Type): Type {
+  return item;
+}
+
+identity<number>(7);
+identity<string>('hello');
+
+//Common Language
+function identity1<T>(item: T): T {
+  return item;
+}
+
+// -------------------------------
+
+// !! Writing Another Generic Function !!
